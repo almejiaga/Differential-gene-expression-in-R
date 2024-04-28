7a9171e7a8b56a9387ba9ecb0c047913bdeb99aa
almejiaga <124840761+almejiaga@users.noreply.github.com>
2024-04-28 11:35:42 -0500

    Update DEGs.R
    Adding arguments for running from the command line, saving normalized counts and also adding PCA plot and volcano plot in proper formatting

--- DEGs.R
+++ DEGs.R
@@ -1,18 +1,54 @@
-#Reading the gene expression matrix
-countTable= read.table("macrofagoshumanos_counts.txt", header=T,row.names=1)
-#removing unwanted columns from FeatureCounts output
-countTable1 = countTable [,6:26]
-countTable1 <- countTable1[,sort(colnames(countTable1))]
-#organizing the names of the columns
-vector4 <- seq(1:84)
-colnames(countTable1) <- vector4[64:84]
+library(argparse)
+library(DESeq2)
+library(ggplot2)
+# Create argument parser
+parser <- ArgumentParser(description = "Script for differential expression analysis")
 
-#reading the factors table
-colData= read.table ("Factors.txt", header=T)
-#Filter to include only two groups for analysis
-colData2 <- dplyr::filter(colData, Condition %in% c("CONTROL", "OBE"))
+# Add arguments
+parser$add_argument("-c", "--count_table", dest = "count_table", 
+                    help = "Path to the gene expression matrix file")
+parser$add_argument("-f", "--factors_table", dest = "factors_table", 
+                    help = "Path to the factors table file")
+parser$add_argument("-c1", "--condition1", dest = "condition1", 
+                    help = "First condition for analysis")
+parser$add_argument("-c2", "--condition2", dest = "condition2", 
+                    help = "Second condition for analysis")
+parser$add_argument("-l", "--label", dest = "label", 
+                    help = "Label to be prepended to output files")
+
+# Parse arguments
+args <- parser$parse_args()
+# Function to construct output file names with label
+construct_output_filename <- function(base_name, label) {
+  if (is.null(label) || label == "") {
+    return(base_name)
+  } else {
+    return(paste(label, base_name, sep = "_"))
+  }
+}
+
+# Example usage of the construct_output_filename function
+label <- args$label
+#define the names of the output
+# Modify file names with label
+DEGs_output_file <- construct_output_filename("significant_DEGs.csv", label)
+volcanoCOVSCA_output_file <- construct_output_filename("volcanoplot.png", label)
+volcanoplotCAVSM1_output_file <- construct_output_filename("enhancedvolcano.png", label)
+
+# Reading the gene expression matrix
+countTable <- read.table(args$count_table, header = TRUE, row.names = 1)
+
+# Reading the factors table
+colData <- read.table(args$factors_table, header = TRUE)
+
+# Filter to include only two groups for analysis
+colData2 <- dplyr::filter(colData, Condition %in% c(args$condition1, args$condition2))
 colData2$sample <- as.character(colData2$sample)
 #filtering count table to exclude genes with less than 32 counts for any treatment
+countTable1 = countTable [,6:26]
+countTable1 <- countTable1[,sort(colnames(countTable1))]
+vector4 <- seq(1:84)
+colnames(countTable1) <- vector4[64:84]
 countTable1<- countTable1[, colData2$sample]
 countTable1[, "max"]= apply(countTable1[, 1:ncol(countTable1)], 1, max)
 countTable1=countTable1[countTable1[,ncol(countTable1)]>32,]
@@ -31,9 +67,9 @@ res3= as.data.frame(res2)
 #filtering to get only significant genes
 DE_genes_pvalue2= subset.data.frame(res3, padj<0.05 & abs(log2FoldChange)>1)
 #saving output to a csv file
-write.csv (DE_genes_pvalue2, file="DEGs_CO_vs_UAL.csv")
+write.csv(DE_genes_pvalue2, file = DEGs_output_file)
 #volcano plot
-png("volcanoCOVSCA.png", units="in", width=5, height=5, res=300)
+png(volcanoCOVSCA_output_file, units = "in", width = 5, height = 5, res = 300)
 alpha <- 0.05 # Threshold on the adjusted p-value
 cols <- densCols(res3$log2FoldChange, -log10(res3$pvalue))
 plot(res3$log2FoldChange, -log10(res3$padj), col=cols, panel.first=grid(),
@@ -51,7 +87,7 @@ dev.off()
 
 #A better volcano plot with another R package
 library(EnhancedVolcano)
-png("volcanoplotCAVSM1.png", units="in", width=8, height=8, res=600)
+png(volcanoplotCAVSM1_output_file, units = "in", width = 8, height = 8, res = 600)
 EnhancedVolcano(res3,
                 lab = rownames(res3),
                 x = 'log2FoldChange',
@@ -62,3 +98,31 @@ EnhancedVolcano(res3,
                 labSize = 6.0)
 
 dev.off()
+#getting the normalized counts
+dds2 <- estimateSizeFactors(dds2)
+sizeFactors(dds2)
+normalized_counts <- counts(dds2, normalized=TRUE)
+all_Z=t(scale(t(normalized_counts)))
+normalized_counts_file <- construct_output_filename("normalized_counts.png", label)
+write.csv(normalized_counts, file = normalized_counts_file)
+#getting the PCA plot
+pca <- prcomp(t(all_Z))
+pca.var1a <- pca$sdev^2
+pca.var.per <- round(pca.var1a/sum(pca.var1a)*100, 1)
+pca.data <- data.frame(Sample=rownames(pca$x),
+                       X=pca$x[,1],
+                       Y=pca$x[,2],
+                       Group=colData2$Condition)
+pca.data
+PCA_output_file <- construct_output_filename("PCAplot.png", label)
+png(PCA_output_file, units="in", width=8, height=8, res=600)
+ggplot(data=pca.data, aes(x=X, y=Y)) +
+  geom_point(aes(colour= Group), size= 5 ) +
+  geom_text(aes(label=Sample)) +
+  xlab(paste("PC1 - ", pca.var.per[1], "%", sep="")) +
+  ylab(paste("PC2 - ", pca.var.per[2], "%", sep="")) +
+  ggtitle("My PCA Graph")
+dev.off()
+
+
+
